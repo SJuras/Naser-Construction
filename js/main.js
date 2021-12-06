9a575310528bf1d400a47e535ba3179dede77e46
SJuras <jurasstpko@gmailcom>
2021-12-06 19:02:02 +0100

    v1.2.1 scroll to top button and smooth scroll scripts added.

--- js/main.js
+++ js/main.js
@@ -13,3 +13,18 @@ menubar.onclick = () => {
 searchbar.onclick = () => {
     search.classList.toggle('active');
 }
+
+// scroll up button
+const btnScrollTop = document.querySelector('#btnScrollTop');
+window.addEventListener("scroll", function(){
+  btnScrollTop.classList.toggle("visible", window.scrollY > 100);
+});
+
+btnScrollTop.addEventListener("click", function(){
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+// scroll up button END
